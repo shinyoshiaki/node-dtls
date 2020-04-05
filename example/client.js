e96fa32824baae9dac40c4a09d6e7a4b1e0d691a
shinyoshiaki <shinyoshiaki2012@gmail.com>
2020-04-05 18:14:50 +0900

    works

--- example/client.js
+++ example/client.js
@@ -1,16 +1,14 @@
 "use strict";
 
 var dtls = require("../");
-var fs = require("fs");
 
 dtls.setLogLevel(dtls.logLevel.FINE);
-var pem = fs.readFileSync("server.pem");
 
-var client = dtls.connect(24125, "localhost", "udp4", function() {
+var client = dtls.connect(4433, "localhost", "udp4", function () {
     client.send(new Buffer("foo\n"));
 });
 
-client.on("message", function(msg) {
+client.on("message", function (msg) {
     console.log("Received application data");
     console.log(msg);
 });
