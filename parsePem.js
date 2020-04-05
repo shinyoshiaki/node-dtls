275cefde41e82a0773e824216ab7de4f90367572
shinyoshiaki <shinyoshiaki2012@gmail.com>
2020-04-05 18:08:50 +0900

    test pass

---
+++ parsePem.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+
+exports.parsePrivate = (path) => {
+    const pem = fs.readFileSync(path).toString();
+    const arr = pem.split("-----").filter((v) => {
+        if (v) {
+            if (v === "\n") return false;
+            return true;
+        }
+        return false;
+    });
+    const key1 = arr.slice(0, 2);
+    const key2 = arr.slice(3, 5);
+
+    const beginHeader = "BEGIN PRIVATE KEY";
+    const endFooter = "END PRIVATE KEY";
+
+    let key = "";
+    if (key1[0] === beginHeader) [, key] = key1;
+    else [, key] = key2;
+
+    return `-----${beginHeader}-----${key}-----${endFooter}-----`;
+};
+
+exports.parseCert = (path) => {
+    const pem = fs.readFileSync(path).toString();
+    const arr = pem.split("-----").filter((v) => {
+        if (v) {
+            if (v === "\n") return false;
+            return true;
+        }
+        return false;
+    });
+    const key1 = arr.slice(0, 3);
+    const key2 = arr.slice(3, 6);
+
+    const beginHeader = "BEGIN CERTIFICATE";
+    const endFooter = "END CERTIFICATE";
+
+    let key = "";
+    if (key1[0] === beginHeader) [, key] = key1;
+    else [, key] = key2;
+
+    return `-----${beginHeader}-----${key}-----${endFooter}-----`;
+};
